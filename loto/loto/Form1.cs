e287250087d793032acdb55cedc699d13f035bfe
ramazan56 <54182580+ramazan56@users.noreply.github.com>
2020-11-19 23:55:23 +0300

    dd

---
+++ loto/loto/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace loto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int[] dizi = new int[6];
+            Random rdm = new Random();
+            int tt=0;
+            label1.Text = textBox1.Text+" :";
+            for (int i = 0; i < 6; i++)
+            {
+                tt = rdm.Next(1, 90);
+                dizi[i] = tt;
+
+                label1.Text += "-"+dizi[i].ToString();
+            }
+
+        }
+    }
+}
